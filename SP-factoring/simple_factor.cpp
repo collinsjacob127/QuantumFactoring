0f1a65e000cdb1f5f88117d2bbde94f4dc7d0669
collinsjacob127 <collinsjacob127@gmail.com>
2025-03-27 20:08:11 -0700

    Input validation

--- SP-factoring/simple_factor.cpp
+++ SP-factoring/simple_factor.cpp
@@ -397,6 +397,7 @@ int main(int argc, char *argv[]) {
   if (z < 9 || z % 2 == 0) { printf("Invalid input for z\n"); }
   if (sqrt(z) == (int) sqrt(z)) { 
     printf("Invalid SP: square semiprime is trivial.\n");
+    exit(EXIT_FAILURE);
   }
   run_SP_factor(z);
 }
